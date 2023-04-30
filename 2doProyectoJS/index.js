f811431cf8f0deab011ea0328df2a9e825fccaa9
Kevin Lautaro Barraza <132170594+Kusuo7K@users.noreply.github.com>
2023-04-30 19:54:39 -0300

    "entrega de la segunda actividad"

---
+++ 2doProyectoJS/index.js
@@ -0,0 +1,13 @@
+const nombre = "Kevin";
+const edad = 18;
+const Desarrollador = true;
+const FechaNacimiento = new Date(2005,0,26);
+const LibroFavorito = {
+    titulo: "El Mundo de Sofía",
+    autor: "Jostein Gaarder",
+    fecha: "1991",
+    url: "https://es.wikipedia.org/wiki/El_mundo_de_Sofía",
+}
+
+const lista = [nombre,edad, Desarrollador, FechaNacimiento,LibroFavorito ];
+console.log(lista);
