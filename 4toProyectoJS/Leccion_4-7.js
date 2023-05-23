7d96601edfa50ae528c3fe47e1f98030af37b88a
Kevin Lautaro Barraza <132170594+Kusuo7K@users.noreply.github.com>
2023-05-23 18:59:48 -0300

    Entrega de actividad 4

---
+++ 4toProyectoJS/Leccion_4-7.js
@@ -0,0 +1,11 @@
+
+var nombre = "Kevin";
+var apellido = "Barraza";
+var estudiante = `${nombre} ${apellido}`;
+var estudianteMayus = estudiante.toLocaleUpperCase();
+var estudianteMinus = estudiante.toLocaleLowerCase();
+var cantLet = estudiante.length;
+var pln = nombre.charAt(0);
+var ula = apellido.charAt(6);
+var elimEs = estudiante.replace(" ","");
+var NomEnEst = estudiante.includes(nombre)
